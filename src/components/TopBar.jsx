bf82abee816ca96c4de8e3d5823de480852a082a
Rahul Hoque <rhoque@umich.edu>
2024-05-02 17:54:17 -0400

    initial commit

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const TopBar = () => {
+    return (
+        <div>
+            <div>
+                <h1>Home</h1>
+            </div>
+            <div>
+                <h1>About</h1>
+            </div>
+            <div>
+                <h1>Projects</h1>
+            </div>
+            <div>
+                <h1>Blog</h1>
+            </div>
+            <div>
+                <h1>Contact</h1>
+            </div>
+        </div>
+    )
+}
+
+export default TopBar
